ed091674494ab3309c88442ad249df8749465ead
Dhruv Kumar <71509305+Futurist-Forever@users.noreply.github.com>
2023-03-03 19:39:07 +0530

    Create program4.c

---
+++ Programs/program4.c
@@ -0,0 +1,12 @@
+/* WAP in C to print a triangle structure */
+#include <stdio.h>
+
+int main()
+{
+    printf("\n         *\n");
+    printf("\n       *   *\n");
+    printf("\n     *       *\n");
+    printf("\n   *           *\n");
+    printf("\n * * * * * * * * *\n");
+    return 0;
+}
