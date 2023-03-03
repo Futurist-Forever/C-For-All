b5d020ddf8ff3bdff1147a1a7f9cd8fff33c2c36
Dhruv Kumar <71509305+Futurist-Forever@users.noreply.github.com>
2023-03-03 19:53:48 +0530

    Create program19.c

---
+++ Programs/program19.c
@@ -0,0 +1,18 @@
+/* WAP to convert given second into its equivalent
+hour, minute and second as per the following format.
+Example. 7560 second = 2 Hour, 27 Minute and 40 Second */
+
+#include <stdio.h>
+
+int main()
+{
+    long sec1, sec2, hr, min, t;
+    printf("\nEnter time in seconds : ");
+    scanf("%ld", &sec1);
+    hr = sec1 / 3600;
+    t = sec1 % 3600;
+    min = t / 60;
+    sec2 = t % 60;
+    printf("\n\n%ld second = %ld Hour %ld Minute and %ld Second\n", sec1, hr, min, sec2);
+    return 0;
+}
