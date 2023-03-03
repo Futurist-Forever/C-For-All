524074f42f09c8cb3d7bb919394007a59248def3
Dhruv Kumar <71509305+Futurist-Forever@users.noreply.github.com>
2023-03-03 19:40:12 +0530

    Create program6.c

---
+++ Programs/program6.c
@@ -0,0 +1,12 @@
+/* WAP in C to print the structure of a circle */
+#include <stdio.h>
+
+int main()
+{
+    printf("   * * * \n");
+    printf(" *       * \n");
+    printf(" *       * \n");
+    printf(" *       * \n");
+    printf("   * * * \n");
+    return 0;
+}
