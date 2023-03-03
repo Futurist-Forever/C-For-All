d6d7340f7d1774c68ef540e07bb298d9f43171fa
Dhruv Kumar <71509305+Futurist-Forever@users.noreply.github.com>
2023-03-03 19:45:17 +0530

    Create program8.c

---
+++ Programs/program8.c
@@ -0,0 +1,15 @@
+/* WAP to print "IIT" using the character '*' */
+#include <stdio.h>
+
+int main()
+{
+    printf("\n\n");
+    printf("* * * * *  * * * * *  * * * * * * *\n");
+    printf("    *          *            *\n");
+    printf("    *          *            *\n");
+    printf("    *          *            *\n");
+    printf("    *          *            *\n");
+    printf("    *          *            *\n");
+    printf("* * * * *  * * * * *        *\n");
+    return 0;
+}
