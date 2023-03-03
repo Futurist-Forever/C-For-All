1d2f63b48b8e17738d5ac1bb46e898891802c2b5
Dhruv Kumar <71509305+Futurist-Forever@users.noreply.github.com>
2023-03-03 19:39:41 +0530

    Create program5.c

---
+++ Programs/program5.c
@@ -0,0 +1,16 @@
+/* WAP in C to print a diamond shape */
+#include <stdio.h>
+
+int main()
+{
+    printf("\n         *\n");
+    printf("\n       *   *\n");
+    printf("\n     *       *\n");
+    printf("\n   *           *\n");
+    printf("\n *               *\n");
+    printf("\n   *           *\n");
+    printf("\n     *       *\n");
+    printf("\n       *   *\n");
+    printf("\n         *\n");
+    return 0;
+}
