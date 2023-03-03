3ca12276d94e77fd9c3403b07269d046bfbf54cb
Dhruv Kumar <71509305+Futurist-Forever@users.noreply.github.com>
2023-03-03 19:38:20 +0530

    Create program3.c

---
+++ Programs/program3.c
@@ -0,0 +1,11 @@
+/* Write a program in C to print a rectangle */
+#include <stdio.h>
+
+int main()
+{
+    printf("\n * * * * * * * * * * \n");
+    printf("\n *                 * \n");
+    printf("\n *                 * \n");
+    printf("\n * * * * * * * * * * \n");
+    return 0;
+}
