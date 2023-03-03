3a14925fc42632135945512765849d93380eab98
Dhruv Kumar <71509305+Futurist-Forever@users.noreply.github.com>
2023-03-03 19:37:40 +0530

    Create program2.c

---
+++ Programs/program2.c
@@ -0,0 +1,10 @@
+/* Write a program in C to print your details */
+#include <stdio.h>
+
+int main()
+{
+    printf("\n Name\t\t: Your Name \n");
+    printf("\n Roll\t\t: Your Roll No \n");
+    printf("\n Address\t: Your Address \n");
+    return 0;
+}
