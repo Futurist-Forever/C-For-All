cc271cefe7e41c045b0cac68f73f87b38867eb87
Dhruv Kumar <71509305+Futurist-Forever@users.noreply.github.com>
2023-03-03 19:59:15 +0530

    Create program27.c

---
+++ Programs/program27.c
@@ -0,0 +1,14 @@
+/* WAP to test whether a number entered through keyboard is ODD or EVEN */
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("\nEnter a number: ");
+    scanf("%d", &a);
+    if ((a % 2) == 0)
+        printf("\n%d is even\n", a);
+    else
+        printf("\n%d is odd\n", a);
+    return 0;
+}
