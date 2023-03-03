188d28e2d8bd1810e7269616466b5af146be7368
Dhruv Kumar <71509305+Futurist-Forever@users.noreply.github.com>
2023-03-03 19:40:44 +0530

    Create program7.c

---
+++ Programs/program7.c
@@ -0,0 +1,20 @@
+/* WAP in C to print the structure of Indian flag */
+#include <stdio.h>
+
+int main()
+{
+    printf(" * * * * * * * * * * * * * * * * * *\n");
+    printf(" *                                 *\n");
+    printf(" *                                 *\n");
+    printf(" *                                 *\n");
+    printf(" * * * * * * * * * * * * * * * * * *\n");
+    printf(" *                *                *\n");
+    printf(" *              *   *              *\n");
+    printf(" *                *                *\n");
+    printf(" * * * * * * * * * * * * * * * * * *\n");
+    printf(" *                                 *\n");
+    printf(" *                                 *\n");
+    printf(" *                                 *\n");
+    printf(" * * * * * * * * * * * * * * * * * *\n");
+    return 0;
+}
